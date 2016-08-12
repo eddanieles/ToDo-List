3a3d4313a907258535f5ba08af9b2ac8dfe84b82
eddanieles <ed.danieles@yahoo.com>
2016-08-12 00:04:20 -0400

    able to add to server

---
+++ main.js
@@ -0,0 +1,73 @@
+var site = "https://fathomless-woodland-51903.herokuapp.com"
+
+$(document).ready(function(){
+/*
+  data.forEach(function(item) {
+    var to_do = $(`<li>
+    ${item.attributes.todo}<input type="checkbox" class="checkbox" ${item.attributes.isComplete ? "checked" : ""}>
+    <button type="button" name="button" class="delete">Delete</button>
+    </li>`);
+
+    $("#to_do_list").append(to_do);
+  })
+*/
+
+  $.getJSON({
+    url: `${site}/todos`,
+    headers: {
+      "Authorization": "Token token=supadupasecret"
+    },
+    success: (response) => {
+      var todoHTML = response.data.map((item) => `<li data-id=${item.id}>
+      ${item.attributes.todo}<input type="checkbox" class="checkbox" ${item.attributes.isComplete ? "checked" : ""}>
+      <button type="button" name="button" class="delete">Delete</button>
+      </li>`);
+
+      $("#to_do_list").append(todoHTML);
+
+      var countChecked = function() {
+      var n = $("li").length - $("input:checked").length;
+      $( "#tasks_left" ).text( n + " tasks" + (n === 1 ? " is" : " are") + " left to do!" );
+      };
+      countChecked();
+      $( "input[type=checkbox]" ).on( "click", countChecked );
+      }
+  });
+
+  $("form").submit(function(event){
+    $.post({
+      url: `${site}/todos`,
+      headers: {
+        "Authorization": "Token token=supadupasecret"
+      },
+      data: $(this).serialize(),
+      success: function(response){
+        $("#to_do_list li:last").data("id", response.data.id)
+      }
+    });
+
+    var todoHTML = `<li>${$(this).find("input").val()}<input type="checkbox" ${this.attributes.isComplete ? "checked" : ""}>
+    <button type="button" name="button" class="delete">Delete</button>
+    </li>`;
+    $("#to_do_list").append(todoHTML);
+
+    $("input").val("");
+    event.preventDefault();
+  })
+
+  $("#to_do_list").on("click", ".delete", function(event){
+    var that = $(this);
+    $.ajax({
+      url: `${site}/todos/${that.parent().data("id")}`,
+      type: "DELETE",
+      headers: {
+        "Authorization": "Token token=supadupasecret"
+      },
+      success: function(data){
+        that.parent().remove()
+      }
+    })
+  })
+
+
+})
